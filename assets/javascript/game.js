0446c540096518ca81b1aeefb4f4cff48f874a30
Kaysic <michaeldaq@mail.com>
2016-10-27 18:32:46 -0400

    added reset function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,58 +1,83 @@
 
 
 $(document).ready(function(){
+	var randomNum = 0;
+	var mainScore = 0;
+	var min = 0;
+	var max = 0;
+	var greenVal = 0;
+	var blueVal = 0;
+	var redVal = 0;
+	var yellowVal = 0;
 	//function for wining or losing the game
+	function reset(){
+		randomScore();
+		randomVal();
+		mainScore = 0;
+		$('.score').text('Score: ' + mainScore);
+
+	}
 	function winLoss(){
-		if (Mainscore === randomNum){
-				alert("you win")
-			} else if (Mainscore > randomNum){
-				alert("you lose")
+		if (mainScore === randomNum){
+				reset();
+				alert("You Win");
+
+			} else if (mainScore > randomNum){
+				reset();
+				alert('You Lose');
 			} else {
 				// do nothing
 			}
 	}
-	var Mainscore = 0;
 	//Selecting and placing Random Number for .randomNumber in html.
-	var min = 19;
-	var max = 120;
-	var randomNum = Math.round(Math.random() * (max - min + 1)) + min;
+	function randomScore(){
+	min = 0;
+	max = 0;
+	min = 19;
+	max = 120;
+	randomNum = Math.round(Math.random() * (max - min + 1)) + min;
 	$(".randomNum").html("Desired Score: " + randomNum);
+	}
 
 	//Selecting and assigning random values to Rupees in html.
-		var greenVal = Math.round(Math.random() * (11 + 1)) + 1;
-		var blueVal = Math.round(Math.random() * (11 + 1)) + 1;
-		var redVal = Math.round(Math.random() * (11 + 1)) + 1;
-		var yellowVal = Math.round(Math.random() * (11 + 1)) + 1;
-		console.log(greenVal);
-		console.log(blueVal);
-		console.log(redVal);
-		console.log(yellowVal);
-
+	function randomVal(){
+		greenVal=0;
+		blueVal=0;
+		redVal=0;
+		yellowVal=0;
+		greenVal = Math.round(Math.random() * (11 + 1)) + 1;
+		blueVal = Math.round(Math.random() * (11 + 1)) + 1;
+		redVal = Math.round(Math.random() * (11 + 1)) + 1;
+		yellowVal = Math.round(Math.random() * (11 + 1)) + 1;
 		$(".greenbtn").val(greenVal);
 		$(".bluebtn").val(blueVal);
 		$(".redbtn").val(redVal);
 		$(".yellowbtn").val(yellowVal);
+}
+
+	randomScore();
+	randomVal();
 
 		//click function for buttons
 		$(".greenbtn").click(function(){
-			Mainscore = Mainscore + greenVal;
-			$(".score").text("Score: " + Mainscore);
+			mainScore = mainScore + greenVal;
+			$(".score").text("Score: " + mainScore);
 			winLoss();
 		});
 		$(".bluebtn").click(function(){
-		 Mainscore = Mainscore + blueVal;
-			$(".score").text("Score: " + Mainscore);
+		 mainScore = mainScore + blueVal;
+			$(".score").text("Score: " + mainScore);
 			winLoss();
 		});
 		$(".redbtn").click(function(){
-			Mainscore = Mainscore + redVal;
-			$(".score").text("Score: " + Mainscore);
+			mainScore = mainScore + redVal;
+			$(".score").text("Score: " + mainScore);
 			winLoss();
 		});
 
 		$(".yellowbtn").click(function(){
-			Mainscore = Mainscore + yellowVal;
-			$(".score").text("Score: " + Mainscore);
+			mainScore = mainScore + yellowVal;
+			$(".score").text("Score: " + mainScore);
 			winLoss();
 		});
 });
